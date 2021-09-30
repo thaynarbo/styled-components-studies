f0dd03313f296697b225cbfffd75b857de2ddfbb
thaynar96 <thaynarbo@gmail.com>
2021-09-30 16:52:33 -0300

    pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import { PageLayout } from 'components/common';
+
+const Home = () => {
+	return (
+		<PageLayout>
+			<div>Home</div>
+		</PageLayout>
+	);
+};
+
+export default Home;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,11 @@
+import { PageLayout } from 'components/common';
+
+const Login = () => {
+	return (
+		<PageLayout>
+			<div>Login</div>
+		</PageLayout>
+	);
+};
+
+export default Login;
