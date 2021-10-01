6c3b83cfafbb7031ef5478395fca982612e7b49e
thaynar96 <thaynarbo@gmail.com>
2021-10-01 11:43:13 -0300

    password, input and spinner component

---
+++ src/components/common/PasswordInput.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Input } from './Input';
+
+const PasswordInputWrapper = styled.div`
+	display: flex;
+	/* ~ is used to select the next sibling */
+	~ div {
+		margin-bottom: 8px;
+	}
+`;
+
+const PasswordInputStyled = styled(Input).attrs(() => ({
+	type: 'password',
+	placeholder: 'Password',
+}))`
+	border-top-right-radius: 0;
+	border-bottom-right-radius: 0;
+`;
+const ToggleButton = styled.div`
+	height: 40px;
+	border: 1px solid #ccc;
+	box-sizing: border-box;
+	font-size: 0.9em;
+	display: flex;
+	padding: 8px;
+	border-left: 0;
+	border-top-right-radius: 4px;
+	border-bottom-right-radius: 4px;
+	background: white;
+	font-weight: bold;
+	cursor: pointer;
+	user-select: none;
+	color: black;
+`;
+
+export const PasswordInput = (props) => {
+	const [showPassword, setShowPassword] = useState(false);
+	return (
+		<>
+			<PasswordInputWrapper>
+				<PasswordInputStyled {...props} />
+				<ToggleButton
+					onClick={() => {
+						setShowPassword(!showPassword);
+					}}
+				>
+					{showPassword ? 'Hide' : 'Show'}
+				</ToggleButton>
+			</PasswordInputWrapper>
+			<div>{showPassword ? props.value : ''}</div>
+		</>
+	);
+};
