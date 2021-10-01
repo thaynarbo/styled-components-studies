dd859bf3439a5b135f6a3c44aad47668d3014b08
thaynar96 <thaynarbo@gmail.com>
2021-10-01 11:38:15 -0300

    form styling and loading spinner

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,92 @@
-import { PageLayout } from 'components/common';
+import { useState, useEffect } from 'react';
+import {
+	PageLayout,
+	Input,
+	PasswordInput,
+	Button,
+	Spinner,
+} from 'components/common';
+import styled from 'styled-components';
 
+const Form = styled.form`
+	width: 100%;
+	max-width: 400px;
+	background: white;
+	border: 1px solid #eee;
+	padding: 16px;
+	box-sizing: border-box;
+	color: black;
+	border-radius: 4px;
+	.alt-text {
+		text-align: center;
+		margin: 10px 0;
+	}
+`;
+
+let timeout;
 const Login = () => {
+	const [formFields, setFormFields] = useState({
+		username: '',
+		password: '',
+	});
+	const [loading, setLoading] = useState(false);
+
+	const handleInputChange = (e) => {
+		e.persist();
+		setFormFields((s) => ({
+			...s,
+			[e.target.name]: e.target.value,
+		}));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		timeout = setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	};
+
+	useEffect(() => {
+		return () => {
+			if (timeout) {
+				clearTimeout(timeout);
+			}
+		};
+	}, []);
 	return (
 		<PageLayout>
-			<div>Login</div>
+			<h1>Login</h1>
+			<Form onSubmit={handleSubmit}>
+				{loading ? (
+					<Spinner />
+				) : (
+					<>
+						<Input
+							value={formFields.username}
+							onChange={handleInputChange}
+							type='text'
+							name='username'
+							placeholder='Username'
+						/>
+						<PasswordInput
+							value={formFields.password}
+							onChange={handleInputChange}
+							name='password'
+						/>
+					</>
+				)}
+				<Button large type='submit' disabled={loading}>
+					{loading ? 'Loading..' : 'Login'}
+				</Button>
+				{!loading && (
+					<>
+						<div className='alt-text'>or</div>
+						<Button secondary type='button'>
+							Register
+						</Button>
+					</>
+				)}
+			</Form>
 		</PageLayout>
 	);
 };
